07b9f470636b06f728d084de47798b9cfd1e3720
millo74x <millo74x@gmail.com>
2016-04-19 10:50:05 +0200

    add msg variable

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 
-//package test;
-
 public class Hello
 {
+  private static String msg = "Hello Java\n";
+
   public static void main(String[] args)
   {
-    System.out.println("Hello World!\n");
+    System.out.println( msg );
   }
 }
